9a7d5fd3db0f214157ae0cb2ec2cd91ffac19803
Christian Lobarede <ch.lobarede@gmail.com>
2021-07-13 17:53:09 -0400

    Primer react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+      <h2>Cosas por hacer</h2>
+      <ul className="list">
+        <li>Aprender React</li>
+        <li>Escalar el Everest</li>
+        <li>Correr un Maraton</li>
+      </ul>
     </div>
   );
 }
